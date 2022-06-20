3f6901b4e0ba3e1f2c6d6caeae2d7dbd482022f6
LoicNG <loic@reality-academy.fr>
2022-06-20 17:18:34 +0200

    initialize routing : in progress

---
+++ src/views/authentication/RegistrationView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Inscription" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'RegistrationView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
