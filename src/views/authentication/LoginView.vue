3d9a386ae9b57b77d102a4d1c7127b1ad1e56e8f
LoicNG <loic@reality-academy.fr>
2022-06-21 16:12:35 +0200

    global css file : ok

--- src/views/authentication/LoginView.vue
+++ src/views/authentication/LoginView.vue
@@ -16,62 +16,30 @@
       </VCol>
     </VRow>
     <VRow justify="center" align="center" style=" margin-top: 10%;">
-      <VCol cols="3">
-        <label for="" class="label-text">E-MAIL</label>
-      </VCol>
       <VCol>
+        <label for="" class="label-text">E-MAIL</label>
         <input class="input" type="email" placeholder="E-mail" required v-model="email" />
       </VCol>
     </VRow>
     <VRow justify="center" align="center">
-      <VCol cols="3">
-        <label for="" class="label-text">PASSWORD</label>
-      </VCol>
       <VCol>
-        <input class="input" type="text" placeholder="Password" required v-model="password" />
+        <label for="" class="label-text">PASSWORD</label>
+        <input class="input" type="password" placeholder="Password" required v-model="password" />
       </VCol>
     </VRow>
     <VRow justify="center" style="margin-top: 10%;">
-      <VBtn width="100%" rounded large block height="40px" style="color:white; background: linear-gradient(#0000A3, #5C5CFF);">
+      <VBtn rounded elevation="3" block height="50px"
+        style="color:white; background: linear-gradient(#0000A3, #5C5CFF);">
         CONNEXION
       </VBtn>
     </VRow>
   </VContainer>
 </template>
 
-<style lang="scss" scoped>
-.label-text {
-  color: #a2a2ba,
-}
-
-.input {
-  border: solid 0.1em #a2a2ba;
-  text-align: center;
-  width: 100%;
-  height: 40px;
-  border-radius: 10px;
-}
-
-input::placeholder {
-  color: #aaaaba;
-}
-
-.layout {
-  background: linear-gradient(#0000A3, #5C5CFF);
-  margin-bottom: 7%;
-  height: 30px;
-}
-</style>
-
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'LoginView',
-  // components: {
-  //   HelloWorld
-  // }
 
   data() {
     return {
