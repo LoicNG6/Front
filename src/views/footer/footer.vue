81d366c5e0e790c05ad414ac32189edeeff86dd4
LoicNG <loic@reality-academy.fr>
2022-06-22 23:16:40 +0200

    footer version 2

--- src/views/footer/footer.vue
+++ src/views/footer/footer.vue
@@ -1,45 +1,32 @@
 <template>
-  <v-footer
-    color="primary lighten-1"
-    padless
-  >
-    <v-row
-      justify="center"
-      no-gutters
-    >
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        text
-        rounded
-        class="my-2"
-      >
-        {{ link }}
-      </v-btn>
-      <v-col
-        class="primary lighten-2 py-4 text-center white--text"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-col>
-    </v-row>
+  <v-footer color="primary lighten-1" padless style="
+    position: absolute;
+    bottom: 0;
+    width: 100%;">
+      <v-row justify="center" class="pt-4">
+        <v-btn v-for="link in links" :key="link" color="white" text rounded>
+          {{ link }}
+        </v-btn>
+        <v-col cols="12" class="primary lighten-2 text-center white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
   </v-footer>
 </template>
 
 <script>
-  export default {
+export default {
 
-    name: 'HomeView',
-    data: () => ({
-      links: [
-        'Home',
-        'About Us',
-        'Team',
-        'Services',
-        'Blog',
-        'Contact Us',
-      ],
-    }),
-  }
+  name: 'HomeView',
+  data: () => ({
+    links: [
+      'Home',
+      'About Us',
+      'Team',
+      'Services',
+      'Blog',
+      'Contact Us',
+    ],
+  }),
+}
 </script>
