4d3905607d86bee6575de7ecbb5e241e422849b3
LoicNG <loic@reality-academy.fr>
2022-06-23 09:31:24 +0200

    bugfix: footerView + aboutView

--- src/views/footer/footer.vue
+++ src/views/footer/footer.vue
@@ -1,26 +1,14 @@
 <template>
-  <v-footer
-    color="primary lighten-1"
-    padless
-  >
-    <v-row
-      justify="center"
-      no-gutters
-    >
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        text
-        rounded
-        class="my-2"
-      >
+  <v-footer color="primary lighten-1" padless style="
+    position:absolute;
+   bottom:0;
+   width:100%;
+   ">
+    <v-row justify="center" no-gutters>
+      <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">
         {{ link }}
       </v-btn>
-      <v-col
-        class="primary lighten-2 py-4 text-center white--text"
-        cols="12"
-      >
+      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
         {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
       </v-col>
     </v-row>
@@ -28,18 +16,18 @@
 </template>
 
 <script>
-  export default {
+export default {
 
-    name: 'HomeView',
-    data: () => ({
-      links: [
-        'Home',
-        'About Us',
-        'Team',
-        'Services',
-        'Blog',
-        'Contact Us',
-      ],
-    }),
-  }
+  name: 'HomeView',
+  data: () => ({
+    links: [
+      'Home',
+      'About Us',
+      'Team',
+      'Services',
+      'Blog',
+      'Contact Us',
+    ],
+  }),
+}
 </script>
