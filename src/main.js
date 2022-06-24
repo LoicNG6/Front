380c19c77363202a59ff33ca9eeeeb6c93975287
LoicNG <loic@reality-academy.fr>
2022-06-24 13:02:33 +0200

    merge develop

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+// import Axios from 'axios'
 
 import "@/styles/app.scss"
 
@@ -12,3 +13,5 @@ createApp(App)
   .use(router)
   .use(vuetify)
   .mount('#app')
+
+// app.config.globalProperties.$axios = Axios;
--- src/views/mainPage/Home.vue
+++ src/views/mainPage/Home.vue
@@ -1,50 +1,61 @@
 <template>
   <v-content v-if="$route.name == 'home'">
     <Header></Header>
+
     <v-container fluid fill-height>
       <v-flex xs12 sm6 offset-sm3>
+        <!-- remplacer dans mon carroussel, colors par entertainment -->
+        <v-carousel v-model="change_carroussel">
+          <v-carousel-item v-for="(color, i) in colors" :key="color">
+            <v-sheet :color="color.title" height="100%" tile>
+              <v-row class="fill-height" align="center" justify="center">
+                <div class="text-h2">
+                  Slide {{ i + 1 }}
+                  {{ color.title }}
+                </div>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
+
         <v-card>
-          <v-img src='../../assets/logo.png' aspect-ratio="3"></v-img>
+          <v-card-title primary-title class="blue-text" style="text-align: center; font-size: 1em;">
+            <v-row>
+              <v-col cols="8">
+                <v-row justify="center">
+                  <v-col cols="12">
+                    <h3>Articles</h3>
+                  </v-col>
+                  <v-col>
+                    <span>Retrouve ici, toutes les derniers nouveautés de nos journaliste du monde entier</span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="4" style="border:solid green">
+                    <v-card></v-card>
+                  </v-col>
+                </v-row>
+              </v-col>
+              <v-col cols="3">
+                <h3 class="headline mb-0">
+                  ÉVENEMENTS
+                </h3>
+                <div>
+                  <p>
+                    Quick example of collapsable mobile menu
+                    <tt>&lt;v-toolbar&gt;</tt>, with a
+                    <tt>&lt;v-menu&gt;</tt> for mobile screens.
+                  </p>
+                  <p>
+                    Also shows how to use <tt>.hidden-md-and-up</tt> CSS
+                    classes to appropriately hide/show the menu depending upon
+                    window width.
+                  </p>
 
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-0">
-                ARTICLES
-              </h3>
-              <div>
-                <p>
-                  Quick example of collapsable mobile menu
-                  <tt>&lt;v-toolbar&gt;</tt>, with a
-                  <tt>&lt;v-menu&gt;</tt> for mobile screens.
-                </p>
-                <p>
-                  Also shows how to use <tt>.hidden-md-and-up</tt> CSS
-                  classes to appropriately hide/show the menu depending upon
-                  window width.
-                </p>
-
-                <v-btn flat color="orange">Share</v-btn>
-              </div>
-            </div>
-            <div>
-              <h3 class="headline mb-0">
-                ÉVENEMENTS
-              </h3>
-              <div>
-                <p>
-                  Quick example of collapsable mobile menu
-                  <tt>&lt;v-toolbar&gt;</tt>, with a
-                  <tt>&lt;v-menu&gt;</tt> for mobile screens.
-                </p>
-                <p>
-                  Also shows how to use <tt>.hidden-md-and-up</tt> CSS
-                  classes to appropriately hide/show the menu depending upon
-                  window width.
-                </p>
-
-                <v-btn flat color="orange">Share</v-btn>
-              </div>
-            </div>
+                  <v-btn flat color="orange">Share</v-btn>
+                </div>
+              </v-col>
+            </v-row>
           </v-card-title>
         </v-card>
       </v-flex>
@@ -64,9 +75,47 @@ export default {
     Header,
     Footer,
   },
+  data() {
+    return {
+      change_carroussel: 0,
+      articles: [],
+      events: {},
+      faits_divers: [],
+      colors: [
+        { title: 'primary' },
+        { title: 'secondary' },
+        { title: 'yellow' },
+        { title: 'red' },
+        { title: 'orange' },
+      ],
+    }
+  },
 
+  computed: {
+    entertainment: {
+      get() {
+        return this.faits_divers;
+      },
+      set() {
+        // Les 3 derniers articles
+        return [this.articles[0], this.articles[1], this.articles[2]];
+      }
+    },
+  },
   mounted() {
+    this.getArticle();
+    this.getEvements();
+
     console.log(this.$route);
-  }
+  },
+
+  methods: {
+    getArticle() {
+
+    },
+    getEvements() {
+
+    },
+  },
 }
 </script>
