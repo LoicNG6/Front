19618870234f7b169cf324f5715b5fe1616a855a
LoicNG <loic@reality-academy.fr>
2022-06-24 04:22:22 +0200

    in progress

--- src/views/mainPage/Home.vue
+++ src/views/mainPage/Home.vue
@@ -1,11 +1,25 @@
 <template>
   <v-content v-if="$route.name == 'home'">
     <Header></Header>
+
     <v-container fluid fill-height>
       <v-flex xs12 sm6 offset-sm3>
-        <v-card>
-          <v-img src='../../assets/logo.png' aspect-ratio="3"></v-img>
 
+        <!-- remplacer dans mon carroussel, colors par entertainment -->
+        <v-carousel v-model="change_carroussel">
+          <v-carousel-item v-for="(color, i) in colors" :key="color">
+            <v-sheet :color="color.title" height="100%" tile>
+              <v-row class="fill-height" align="center" justify="center">
+                <div class="text-h2">
+                  Slide {{ i + 1 }}
+                  {{ color.title }}
+                </div>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
+
+        <v-card>
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">
@@ -64,9 +78,47 @@ export default {
     Header,
     Footer,
   },
+  data() {
+    return {
+      change_carroussel: 0,
+      articles: [],
+      events: {},
+      faits_divers: [],
+      colors: [
+        { title: 'primary' },
+        { title: 'secondary' },
+        { title: 'yellow' },
+        { title: 'red' },
+        { title: 'orange' },
+      ],
+    }
+  },
 
+  computed: {
+    entertainment: {
+      get() {
+        return this.faits_divers;
+      },
+      set() {
+        // Les 3 derniers articles
+        return [this.articles[0], this.articles[1], this.articles[2]];
+      }
+    },
+  },
   mounted() {
+    this.getArticle();
+    this.getEvements();
+
     console.log(this.$route);
-  }
+  },
+
+  methods: {
+    getArticle() {
+
+    },
+    getEvements() {
+
+    },
+  },
 }
 </script>
